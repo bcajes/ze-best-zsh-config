0713baf8752da23aea5c0b152c570dd3338a1601
Brian Cajes <brian.cajes@gmail.com>
2013-12-29 14:34:51 -0500

    Update history.zsh

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -1,4 +1,10 @@
-# HISTORY
-HISTSIZE=3000
+#HISTORY
+export HISTSIZE=1000000
+export HISTFILESIZE=1000000000
+# Combine multiline commands into one in history
+shopt -s cmdhist
+# Ignore duplicates, ls without options and builtin commands
+HISTCONTROL=ignoredups
+export HISTIGNORE="&:ls:[bf]g:exit"
 SAVEHIST=3000
 HISTFILE=~/.zsh_history
